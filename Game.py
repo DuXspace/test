0c21f7912a67521aa66a71451df225e4847a7ec7
DuXspace <linkin91@mail.ru>
2020-02-20 23:13:40 +0200

    пишешь сюда сообщение
    Если хочешь пишешь сюда описание и жмякаешь commit changes

---
+++ Game.py
@@ -0,0 +1,12 @@
+from .Citadel import Citadel
+
+
+class Game:
+    def __init__(self):
+        self.citadel = Citadel()
+
+    def start(self):
+        pass
+
+    def show_section_cards(self):
+        pass
